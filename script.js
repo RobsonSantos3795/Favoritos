c8f77592c87bd65272dca3aa7635264f929310fe
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2025-07-15 10:08:11 -0300

    Update script.js
    Atualizando Script

--- script.js
+++ script.js
@@ -35,3 +35,48 @@ function login4() {
 function login1() {
     window.location.href = "https://app.powerbi.com/groups/me/list?experience=power-bi";
 }
+
+function entrou1() {
+    let cor1 = document.getElementById("bt1");
+    cor1.style.background = "green";
+} 
+function saiu1() {
+    let cor1 = document.getElementById("bt1");
+    cor1.style.background = "yellow";
+}
+
+function entrou2() {
+    let cor2 = document.getElementById("bt2");
+    cor2.style.background = "green";
+}
+function saiu2() {
+    let cor2 = document.getElementById("bt2");
+    cor2.style.background = "yellow";
+}
+
+function entrou3() {
+    let cor3 = document.getElementById("bt3");
+    cor3.style.background = "green";
+}
+function saiu3() {
+    let cor3 = document.getElementById("bt3");
+    cor3.style.background = "yellow";
+}
+
+function entrou4() {
+    let cor4 = document.getElementById("bt4");
+    cor4.style.background = "green";
+}
+function saiu4() {
+    let cor4 = document.getElementById("bt4");
+    cor4.style.background = "yellow";
+}
+
+function entrou5() {
+    let cor5 = document.getElementById("bt5");
+    cor5.style.background = "green";
+}
+function saiu5() {
+    let cor5 = document.getElementById("bt5");
+    cor5.style.background = "yellow";
+}
